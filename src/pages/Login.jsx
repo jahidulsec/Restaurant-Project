5edf8f0ed9046ef1563187a7d03a177fe6275441
jahidulsec <jahidul.islam.sec@gmail.com>
2023-06-06 00:12:07 +0600

    add login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <section className='login-section'>
+      <div className="container">
+        <form action="">
+          <h1 className="ff-secondary fw-regular fs-2xl text-dark">
+            Welcome to Little Lemon!
+          </h1>
+          <div className="field">
+            <label htmlFor="username">Username<sup>*</sup></label>
+            <input type="text" name="username" id="username" />
+          </div>
+          <div className="field">
+            <label htmlFor="password">Password <sup>*</sup></label>
+            <input type="password" name="password" id="password" />
+          </div>
+          <div className="btns">
+            <button className='btn-dark'>Login</button>
+            <button className='btn-dark'>Register!</button>
+          </div>
+        </form>
+      </div>
+    </section>
   )
 }
 
