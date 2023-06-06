04d5e79eb88447f5e0ff435254a595fb93513941
jahidulsec <jahidul.islam.sec@gmail.com>
2023-06-06 15:48:38 +0600

    adding login form design

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,13 +8,13 @@ const Login = () => {
           <h1 className="ff-secondary fw-regular fs-2xl text-dark">
             Welcome to Little Lemon!
           </h1>
-          <div className="field">
-            <label htmlFor="username">Username<sup>*</sup></label>
-            <input type="text" name="username" id="username" />
+          <div className="login-field">
+            <label className='login-label' htmlFor="username">Username<sup>*</sup></label>
+            <input type="text" name="username" id="username" placeholder='ex. johndoe' />
           </div>
-          <div className="field">
-            <label htmlFor="password">Password <sup>*</sup></label>
-            <input type="password" name="password" id="password" />
+          <div className="login-field">
+            <label className='login-label' htmlFor="password">Password <sup>*</sup></label>
+            <input type="password" name="password" id="password" placeholder='ex. 1232@153asda' />
           </div>
           <div className="btns">
             <button className='btn-dark'>Login</button>
